ff8c76fa625736145150e3f2ae10eb5b6f7d2766
Rahul Bhilesha <bhilesharahul@gmail.com>
2021-09-09 13:09:30 +0530

    Understand about binding and implement it.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Card from './Card';
-import faker from 'faker';
 
 function App() {
+  const [name, setName] = useState('Rahul Bhilesha');
+  const [showCard, setShowCard] = useState(true);
+  const changeNameHandler = name => setName(name);
+  const changeInputHandler = event => setName(event.target.value);
+  const toggleShowCard = () => setShowCard(!showCard);
+  const buttonMarkup = (
+    <div>
+      <button className="button button2">Yes</button>
+      <button className="button button3">No</button>
+    </div>
+  );
+  const cardMarkup = (
+    <Card avatar={'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80'} name={name} title={'Internal Response Officer'} onChangeName={() => changeNameHandler('Bhilesha Rahul')} onChangeInput={changeInputHandler}>
+      {buttonMarkup}
+    </Card>
+  );
   return (
     <div className="App">
-      <Card avatar={faker.image.avatar()} name={`${faker.name.firstName()} ${faker.name.lastName()}`} title={faker.name.jobTitle()}></Card>
-      <Card avatar={faker.image.avatar()} name={`${faker.name.firstName()} ${faker.name.lastName()}`} title={faker.name.jobTitle()}></Card>
-    </div>
+      <button className="button" onClick={toggleShowCard}>{showCard ? 'Hide Card' : 'Show Card'}</button>
+      {showCard && cardMarkup}
+    </div >
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css';
 
 const Card = (props) => {
+    console.log('props', props);
     return (
         <div className="card">
             <img src={props.avatar} alt="Avatar" style={{ width: '100%' }}></img>
             <div className="container">
                 <h4><b>{props.name}</b></h4>
+                <input type="text" onChange={props.onChangeInput} value={props.name} />
                 <p>{props.title}</p>
+                <p><button className="button" onClick={props.onChangeName}>Change Name</button></p>
+                <div>{props.children}</div>
             </div>
         </div>
     )
