02af214fb4df5526b47b1bb9f71791e23dffeb8e
Rahul Bhilesha <bhilesharahul@gmail.com>
2021-09-09 19:15:16 +0530

    Implmented logic to delete card and styled the components.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
 import React, { useState } from 'react';
 import './App.css';
 import Card from './Card';
+import { ThemeProvider } from 'styled-components';
+import Button from './element/Button';
 
+const theme = {
+  primary: '#4CAF50',
+  secondary: 'Yellow'
+};
 function App() {
-  const [name, setName] = useState('Rahul Bhilesha');
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      name: 'Rahul Bhilesha',
+      title: 'Project Manager',
+      avtar: 'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80'
+    },
+    {
+      id: 2,
+      name: 'Developer Bhilesha',
+      title: 'Designer',
+      avtar: 'https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+    },
+    {
+      id: 3,
+      name: 'Alvero morte',
+      title: 'Developer',
+      avtar: 'https://images.unsplash.com/photo-1573997960962-858fb33936bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80'
+    }
+  ]);
   const [showCard, setShowCard] = useState(true);
-  const changeNameHandler = name => setName(name);
-  const changeInputHandler = event => setName(event.target.value);
   const toggleShowCard = () => setShowCard(!showCard);
-  const buttonMarkup = (
-    <div>
-      <button className="button button2">Yes</button>
-      <button className="button button3">No</button>
-    </div>
-  );
+  const deleteCardHandler = (cardIndex) => {
+    const copy_cards = [...cards];
+    copy_cards.splice(cardIndex, 1);
+    setCards(copy_cards);
+  };
+  const changeNameHandler = (event, id) => {
+    const cardIndex = cards.findIndex(card => card.id === id);
+    const card_copy = [...cards];
+    card_copy[cardIndex].name = event.target.value;
+    setCards(card_copy);
+  }
   const cardMarkup = (
-    <Card avatar={'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80'} name={name} title={'Internal Response Officer'} onChangeName={() => changeNameHandler('Bhilesha Rahul')} onChangeInput={changeInputHandler}>
-      {buttonMarkup}
-    </Card>
+    cards.map((card, index) => <Card
+      avatar={card.avtar}
+      name={card.name}
+      title={card.title}
+      key={card.id}
+      onDelete={() => deleteCardHandler(index)}
+      onChangeName={(event) => changeNameHandler(event, card.id)}
+    />
+    )
   );
   return (
-    <div className="App">
-      <button className="button" onClick={toggleShowCard}>{showCard ? 'Hide Card' : 'Show Card'}</button>
-      {showCard && cardMarkup}
-    </div >
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Button color="secondary" onClick={toggleShowCard} length={cards.length}>{showCard ? 'Hide Cards' : 'Show Cards'}</Button>
+        {showCard && cardMarkup}
+      </div >
+    </ThemeProvider>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,15 +2,14 @@ import React from 'react'
 import './App.css';
 
 const Card = (props) => {
-    console.log('props', props);
     return (
         <div className="card">
             <img src={props.avatar} alt="Avatar" style={{ width: '100%' }}></img>
             <div className="container">
                 <h4><b>{props.name}</b></h4>
-                <input type="text" onChange={props.onChangeInput} value={props.name} />
+                <input type="text" onChange={props.onChangeName} value={props.name} />
                 <p>{props.title}</p>
-                <p><button className="button" onClick={props.onChangeName}>Change Name</button></p>
+                <p><button className="button red" onClick={props.onDelete}>Delete</button></p>
                 <div>{props.children}</div>
             </div>
         </div>
